d451b7f4b73ee04282a65aedc89702ae9276ce72
Drazam <146709444+pierre-mazard@users.noreply.github.com>
2024-11-13 16:29:16 +0100

    update : top 10

--- app.py
+++ app.py
@@ -29,11 +29,24 @@ class AdultMortalityApp:
         fig = px.line(filtered_df, x='year', y='adult mortality', color='country', title='Adult Mortality by Country and Year')
         return fig
 
+class Top10App:
+    def __init__(self, df):
+        self.df = df
+
+    def create_figure(self, selected_column):
+        if not selected_column:
+            return go.Figure()  # Retourner un graphique vide si aucune colonne n'est sélectionnée
+        avg_df = self.df.groupby('country')[selected_column].mean().reset_index()
+        top_10 = avg_df.nlargest(10, selected_column)
+        fig = px.bar(top_10, x='country', y=selected_column, title=f'Top 10 Countries by {selected_column.capitalize()}')
+        return fig
+
 class HealthDataApp:
     def __init__(self, csv_file):
         self.df = pd.read_csv(csv_file)
         self.life_expectancy_app = LifeExpectancyApp(self.df)
         self.adult_mortality_app = AdultMortalityApp(self.df)
+        self.top_10_app = Top10App(self.df)
         self.app = dash.Dash(__name__)
         self.setup_layout()
         self.setup_callbacks()
@@ -57,7 +70,13 @@ class HealthDataApp:
                 multi=True,
                 placeholder='Select Countries'
             ),
-            dcc.Graph(id='line-chart')
+            dcc.Graph(id='line-chart'),
+            dcc.Dropdown(
+                id='top-10-dropdown',
+                options=[{'label': col, 'value': col} for col in self.df.columns if col not in ['country', 'year', 'status']],
+                placeholder='Select Column for Top 10'
+            ),
+            dcc.Graph(id='top-10-chart')
         ])
 
     def setup_callbacks(self):
@@ -72,9 +91,16 @@ class HealthDataApp:
             else:
                 return self.adult_mortality_app.create_figure(selected_countries)
 
+        @self.app.callback(
+            Output('top-10-chart', 'figure'),
+            [Input('top-10-dropdown', 'value')]
+        )
+        def update_top_10(selected_column):
+            return self.top_10_app.create_figure(selected_column)
+
     def run(self):
         self.app.run_server(debug=True)
 
 if __name__ == '__main__':
-    health_data_app = HealthDataApp('raw_data/LifeExpectancyData.csv')
+    health_data_app = HealthDataApp('processed_data/LifeExpectancyData.csv')
     health_data_app.run()
